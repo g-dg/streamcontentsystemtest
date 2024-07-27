3d54f221149f2850bad6d01fe190a868f42bbfbe
Garnet DeGelder <garnet@garnetdg.ca>
2024-07-26 23:41:01 -0400

    Remove transitions and add support setting render delay

--- client/src/views/RenderView.vue
+++ client/src/views/RenderView.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { computed, nextTick, onMounted, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 
 import RootRenderer from "@/components/renderers/RootRenderer.vue";
 
 import { useStateStore, type StateContent } from "@/stores/state";
-import { uuid } from "@/helpers/random";
 
 const stateStore = useStateStore();
 onMounted(stateStore.connect);
@@ -19,100 +18,26 @@ const fontSize = computed(() => {
   const size_query = (route.query["font-size"] as string) ?? null;
   const number_size = size_query?.match(/^\d+(\.\d+)?$/)?.[0];
   if (number_size != null) return `${number_size}pt`;
-  if (size_query != null && size_query.match(/^\d+(\.\d+)?[a-z]*/))
-    return size_query;
+  if (size_query != null) return size_query;
   return DEFAULT_FONT_SIZE;
 });
 
-const SHOW_START_DELAY = 1000;
-const FADE_DURATION = 200;
+const renderDelay = ref(0);
 
-const animationQueue = ref<
-  Array<{
-    id: string;
-    content: StateContent;
-  }>
->([
-  {
-    id: uuid(),
-    content: {
-      background: false,
-    },
-  },
-]);
-const animationQueueElements = ref<Array<HTMLElement>>([]);
+const delayedContent = ref<StateContent>(currentContent.value);
 
-function getElementFromQueueId(id: string) {
-  const index = animationQueue.value.findIndex((renderer) => renderer.id == id);
-  return animationQueueElements.value[index];
-}
-
-async function setContent(content: StateContent) {
-  const id = uuid();
-  animationQueue.value.push({ id, content });
-
-  await nextTick();
-  await new Promise((resolve) => window.setTimeout(resolve, 100));
-
+watch(currentContent, (content) =>
   window.setTimeout(() => {
-    getElementFromQueueId(id).classList.add("fade-show");
-
-    const oldItems = animationQueue.value.slice(0, -1);
-    oldItems.forEach((item) => {
-      const element = getElementFromQueueId(item.id);
-      element.classList.remove("fade-show");
-
-      window.setTimeout(() => {
-        const index = animationQueue.value.findIndex((x) => x.id == item.id);
-        if (index > -1) {
-          animationQueue.value.splice(index, 1);
-        }
-      }, FADE_DURATION);
-    });
-  }, SHOW_START_DELAY);
-}
-
-watch(currentContent, () => setContent(currentContent.value));
+    delayedContent.value = content;
+  }, renderDelay.value)
+);
 </script>
 
 <template>
-  <div style="width: 100%; height: 100%">
-    <TransitionGroup name="fade">
-      <div
-        v-for="item in animationQueue"
-        :key="item.id"
-        ref="animationQueueElements"
-        class="fade"
-        :style="{
-          // transition: `opacity ${FADE_DURATION}ms linear`,
-          width: '100%',
-          height: '100%',
-        }"
-      >
-        <RootRenderer :content="item.content" :font-size="fontSize" />
-      </div>
-    </TransitionGroup>
-  </div>
+  <RootRenderer :content="delayedContent" :font-size="fontSize" />
 </template>
 
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 200ms linear;
-  opacity: 1;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-// .fade {
-//   opacity: 0;
-// }
-// .fade-show {
-//   opacity: 1;
-// }
-</style>
+<style lang="scss" scoped></style>
 
 <style lang="scss">
 body {
