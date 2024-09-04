f48a5039feb3b218b491352bc58d52c70c19ccc3
Garnet DeGelder <garnet@garnetdg.ca>
2024-09-03 22:58:13 -0400

    Move some inline styles to class

--- client/src/components/renderers/RootRenderer.vue
+++ client/src/components/renderers/RootRenderer.vue
@@ -33,32 +33,38 @@ const textShadow = computed(() =>
     <SmallTextRenderer
       :content="content"
       :font-size="fontSize"
-      style="position: absolute; top: 0; left: 0; right: 0; bottom: 0"
+      class="full-size"
     />
     <SubTextRenderer
       :content="content"
       :font-size="fontSize"
-      style="position: absolute; top: 0; left: 0; right: 0; bottom: 0"
+      class="full-size"
     />
     <MainTextRenderer
       :content="content"
       :font-size="fontSize"
-      style="position: absolute; top: 0; left: 0; right: 0; bottom: 0"
+      class="full-size"
     />
     <SongTitleRenderer
       :content="content"
       :font-size="fontSize"
-      style="position: absolute; top: 0; left: 0; right: 0; bottom: 0"
+      class="full-size"
     />
-    <SongRenderer
-      :content="content"
-      :font-size="fontSize"
-      style="position: absolute; top: 0; left: 0; right: 0; bottom: 0"
-    />
+    <SongRenderer :content="content" :font-size="fontSize" class="full-size" />
   </div>
 </template>
 
 <style lang="scss" scoped>
+.full-size {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+}
+
 .renderer {
   width: 100vw;
   height: 100vh;
